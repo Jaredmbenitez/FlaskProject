62da0a0504ce637ed2d8db0130b7dd33adaec4f0
JaredBenitez <Jaredmbenitez@gmail.com>
2021-04-19 20:21:26 -0700

    Dynamic Cart Page and Cleanup

--- FlaskProject.py
+++ FlaskProject.py
@@ -40,13 +40,7 @@ from werkzeug.utils import secure_filename
 from models.Report import Report
 from classes.database import Database
 import json
-
-
-def convertToBinaryData(filename):
-    # Convert digital data to binary format
-    with open(filename, 'rb') as file:
-        blobData = file.read()
-    return blobData
+from functions import *
 
 
 app = Flask(__name__)
@@ -72,6 +66,7 @@ def home():
         description = request.form.get("description")
         price = request.form.get("price")
         posted_by = session['username']
+        title = request.form.get("title")
 
         # Process image into binary data
         image = request.files["inputFile"]
@@ -80,7 +75,7 @@ def home():
         url = convertToBinaryData(url)
 
         # Create new photo object and add to database.
-        newPhoto = Photo(image=url, tags=tags, price=price,
+        newPhoto = Photo(image=url, title=title, tags=tags, price=price,
                          nsfw=nsfw, posted_by=posted_by, times_purchased=times_purchased, num_views=0)
         # Add and commit to database
         db.session.add(newPhoto)
@@ -243,7 +238,14 @@ def shop():
 
 @app.route("/cart")  # cart Page        --------------------------
 def cart():
-    return render_template('cart.html', title="Cart")
+    cartItems = getCartDatabyUsername(session["username"])
+    itemsList = []
+    for item in cartItems:
+        itemInfo = Photo.query.filter_by(photo_id=item.photo_id).first()
+        itemInfo = decodeImageFromObject(itemInfo)
+        itemsList.append(itemInfo)
+
+    return render_template('cart.html', title="Cart", cartData=itemsList)
 # adding stuff to cart branch=
 
 # Login Page, Accepts POST and GET requests --------------------------
@@ -331,160 +333,11 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-##### Extra Functions ######
-# Returns a random photo decoded ready to be caught.
-def generateRandomImage():
-    # Query photo table for all entries
-    obj = Photo.query.all()
-    if len(obj) == 0:
-        return 0
-    # Choose a random entry from the table
-    randomNumber = random.randint(0, len(obj) - 1)
-    # Decode the image
-    randomImage = b64encode(obj[randomNumber].image).decode("utf-8")
-    # image can be caught in html pages by the following:
-    # <img src="data:;base64,{{ image }}"/>
-    return randomImage
-
-# Returns a random Photo Object. Image has been decoded.
 
 
-def generateRandomPhotoObject():
-
-    # Query photo table for all entries
-    obj = Photo.query.all()
-    if len(obj) == 0:
-        return
-    # Choose a random entry from the table
-    randomNumber = random.randint(0, len(obj) - 1)
-    randomPhotoObject = obj[randomNumber]
-    if type(randomPhotoObject.image) == str:
-        tempImage = bytes(randomPhotoObject.image, encoding='utf-8')
-    else:
-        tempImage = b64encode(randomPhotoObject.image)
-    tempImage = tempImage.decode("utf-8")
-    randomPhotoObject.image = tempImage
-    return randomPhotoObject
 
 
-# Generate a number of random Photo Object
-def generateXRandomPhotoObjects(x):
-    objectsList = []
-    for key in range(x):
-        tempObj = generateRandomPhotoObject()
-        # Somehow we need to check here for duplicates
-        objectsList.append(tempObj)
-    return objectsList
 
 
-# FINISHED
-def getUserIdbyUsername(user):
-    # Query to find user ID
-    queryObject = User.query.filter_by(username=user).first()
-    return queryObject.id
-
-# Item Page Wyatt
 
 
-def getPhotoIdBy_____():
-    return 0
-
-# Account page - Alec
-
-
-def getPhotoObjectsByUsername(user):
-    queryObjects = Photo.query.filter_by(posted_by=user).all()
-    for obj in queryObjects:
-        tempImage = obj.image
-        tempImage = b64encode(tempImage).decode("utf-8")
-        obj.image = tempImage
-    return queryObjects
-
-# Cart page - Matthew
-
-
-def getCartDatabyUserID():
-    return 0
-
-
-def getPhotoObjectByPhotoID(id):
-    queryObject = Photo.query.filter_by(photo_id=id).first()
-    tempImage = queryObject.image
-    tempImage = b64encode(tempImage).decode("utf-8")
-    queryObject.image = tempImage  # db.session.commit breaks the page after this line
-    return queryObject
-
-
-def getUserObjectByPhotoID(id):
-    PhotoObject = Photo.query.filter_by(photo_id=id).first()
-    UserObject = User.query.filter_by(username=PhotoObject.posted_by).first()
-    return UserObject
-
-# Returns a User object given the username
-
-
-def getUserInfoByUsername(user):
-    userObject = User.query.filter_by(username=user).first()
-    return userObject
-
-
-def getAllExistingPhotoObjects():
-    queryObjects = Photo.query.all()
-    for obj in queryObjects:
-        tempImage = obj.image
-        tempImage = b64encode(tempImage).decode("utf-8")
-        obj.image = tempImage
-    return queryObjects
-
-
-def incrementView(id):
-
-    db = Database()
-    photoObject = Photo.query.filter_by(photo_id=id).first()
-    newVal = str(photoObject.num_views + 1)
-    sql = ("UPDATE photos SET num_views = " +
-           newVal + " WHERE `photo_id`= " + str(id))
-    db.execute(sql)
-
-
-def createGuestUser():
-    username = "Guest" + str(random.randint(99, 1000))
-    password = "asdfasdf"
-    email = username + " @yahoo.com"
-    GuestUser = User(username=username,
-                     password=encrypt_password(password), email=email)
-    db.session.add(GuestUser)
-    db.session.commit()
-    return GuestUser
-
-
-def addItemToCart(id):
-
-    username = getUserObjectByPhotoID(id).username
-    photoObject = getDecodedImageObjectByPhotoId(id)
-
-    photo_id = photoObject.photo_id
-    price = photoObject.price
-    cart_id = getUserInfoByUsername(session["username"]).id
-
-    cartItem = Cart(posted_by_username=username, photo_id=photo_id)
-
-    newDB = Database()
-    sql = f"INSERT INTO cart (`posted_by_username`, `photo_id`, `cart_id`) VALUES('" + \
-        username + "'," + "'" + str(photo_id) + \
-        "'," + "'" + str(cart_id) + "')"
-    newDB.execute(sql)
-
-    return 1
-
-
-def getDecodedImageObjectByPhotoId(id):
-    queryObject = Photo.query.filter_by(photo_id=id).first()
-
-    if type(queryObject.image) == str:
-        tempImage = bytes(queryObject.image, encoding='utf-8')
-    else:
-        tempImage = b64encode(queryObject.image)
-    tempImage = tempImage.decode("utf-8")
-    queryObject.image = tempImage
-    return queryObject
